ee17bad83c3dc9ca9e80b0f3e4f1a7d03489deaf
xicoosilvaa <184397442+xicoosilvaa@users.noreply.github.com>
2024-11-27 16:07:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# forum_09_18
+ Trabalho Final M17E
---
+++ header.php
@@ -0,0 +1,8 @@
+<div class="my-2 p-5 bg-buzio text-white rounded">
+    <div class="row">
+        <div class="col-sm-8 ">
+            <h1 class="monoton-regular">Fórum NN</h1>
+        </div>
+    </div>
+</div>
+
