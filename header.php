7b6febbfe18f109d3dcd72e5b7c02e67c4a4a684
peixoto-pixel <184397472+peixoto-pixel@users.noreply.github.com>
2024-11-29 10:41:44 +0000

    mudar header

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <div class="my-2 p-5 bg-buzio text-white rounded">
     <div class="row">
         <div class="col-sm-8 ">
-            <h1 class="monoton-regular">Fórum NN</h1>
+            <h1 class="monoton-regular">Fórum 09_18</h1>
         </div>
     </div>
 </div>
